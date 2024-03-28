a24a08f46b736ab2a75bf7277defbe8bc596a5d4
DerManjuel <manuel_herbst@yahoo.de>
2024-03-28 19:36:33 +0100

    Training Six

--- predict_new_images.py
+++ predict_new_images.py
@@ -182,7 +182,7 @@ def display_image(image):
 
 
 # check if directory is present and exit if necessary
-trainNr = '5'
+trainNr = '6'
 path = r"C:\Users\manue\PycharmProjects\ML-for_HoloLens-py3.9\dataset\test\images"
 
 check_dir()
