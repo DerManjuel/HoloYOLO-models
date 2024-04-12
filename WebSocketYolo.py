d08f8b7519056dd25a71c37c80888b8e021fbdbe
Manuel <manuel.herbst@student.uni-luebeck.de>
2024-04-12 11:11:24 +0200

    Changes to server files, converted image to correct image representation format

--- WebSocketYolo.py
+++ WebSocketYolo.py
@@ -1,7 +1,9 @@
 import socket
+import cv2
+import numpy as np
 
 # Set up server
-server_ip = '192.168.159.104'  # '192.168.159.117' # HoloLens Marlon
+server_ip = '192.168.159.114'  # '192.168.159.117' # HoloLens Marlon 192.168.159.104
 server_port = 8889
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((server_ip, server_port))
@@ -28,6 +30,19 @@ try:
 
             print(image_data)
             #print(image_data)
+
+            # Assuming 'imageData' is the byte array received from Unity
+            # Decode the JPEG byte array into an image (assuming it's already in RGB format)
+            #image = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+            # Convert from BGRA to RGB
+            #image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
+
+            # Now, 'image' should be in RGB format, proceed with saving it or any further processing
+            # For example, saving the image
+            #cv2.imwrite('output.jpg', image)
+
+
             # Save the received image data to a file (e.g., "received_photo.jpg")
             with open("received_photo.jpg", "wb") as file:
                 file.write(image_data)
