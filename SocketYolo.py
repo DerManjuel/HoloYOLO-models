d0dbc08b7d1fe3e957db59167fd050d18208d566
Manuel <manuel.herbst@student.uni-luebeck.de>
2024-04-26 12:01:09 +0200

    New App init

--- SocketYolo.py
+++ SocketYolo.py
@@ -3,9 +3,18 @@ import cv2
 import numpy as np
 from ultralytics import YOLO
 import gpu_availablility
+import base64
+
+
+def convert_image_to_byte_array(img):
+    # Convert the OpenCV image to a byte array
+    _, buffer = cv2.imencode('.jpg', img)
+    byte_array = base64.b64encode(buffer)
+    return byte_array
+
 
 # Set up server
-server_ip = '127.0.0.1' # 192.168.159.116
+server_ip = '127.0.0.1' # 192.168.159.116 # Computer IP
 server_port = 8889
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((server_ip, server_port))
@@ -28,7 +37,7 @@ try:
 
         try:
             # Send confirmation to client
-            client_socket.sendall(b"Connected to server.")
+            #client_socket.sendall(b"Connected to server.")
 
             # Receive image data from client
             image_data = b""
@@ -41,14 +50,14 @@ try:
             print(image_data, "  ;  ", type(image_data))
 
             # Save the received image data to a file (e.g., "received_photo.jpg")
-            with open("HoloLens2Prediction/recieved/received_photo.png", "wb") as file:
+            with open("HoloLens2Prediction/recieved/SDC-received_photo.png", "wb") as file:
                 file.write(image_data)
 
-            image = cv2.imread("HoloLens2Prediction/recieved/received_photo.png")
+            image = cv2.imread("HoloLens2Prediction/recieved/SDC-received_photo.png")
 
             # Run inference with the YOLOv8n model on an image
             results = model(image, device=gpu)
-            # Extract bounding boxes, classes and confidence scores
+            # Extract bounding boxes, classes and confidence scores0
             result = results[0]
             bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
             classes = np.array(result.boxes.cls.cpu(), dtype="int")
@@ -78,12 +87,14 @@ try:
                 print(str(cls), str(cls_name))
 
             # save .jpg
-            cv2.imwrite('HoloLens2Prediction/predicted/predictionImage.jpg', image)
+            cv2.imwrite('HoloLens2Prediction/predicted/SDC-predictionImage.jpg', image)
             print('Image prediction saved.')
             print('=========================================================')
 
+            # Convert the image to a byte array
+            byte_array = convert_image_to_byte_array(image)
             # Send response to client
-            client_socket.sendall(b"Image received successfully.")
+            client_socket.sendall(byte_array)
 
         except Exception as e:
             print("Error during communication:", e)
